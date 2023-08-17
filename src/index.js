eeba3f7c8ece3e8720be244120e1ba050677b4d3
parunkov <parunkov@gmail.com>
2023-08-17 16:17:11 +0500

    create diagramm

--- src/index.js
+++ src/index.js
@@ -1,26 +1,49 @@
-// Test import of a JavaScript module
-// import { example } from '@/js/example'
+const colours = ["#EB5757", "#F2994A", "#6FCF97", "#9B51E0", "#2F80ED", "#56CCF2", "#219653", "#F2C94C"];
+const data = [45, 50, 20, 15, 10, 8, 6, 4];
+const radius = [150, 200, 120, 140, 160, 100, 110, 170];
 
-// // Test import of an asset
-// import webpackLogo from '@/images/webpack-logo.svg'
+function drawPieSlice(settings) {
+    let d = "";
 
-// // Test import of styles
-// import '@/styles/index.scss'
+    const firstCircumferenceX = settings.centreX + settings.radius * Math.cos(settings.startAngleRadians);
+    const firstCircumferenceY = settings.centreY + settings.radius * Math.sin(settings.startAngleRadians);
+    const secondCircumferenceX = settings.centreX + settings.radius * Math.cos(settings.startAngleRadians + settings.sweepAngleRadians);
+    const secondCircumferenceY = settings.centreY + settings.radius * Math.sin(settings.startAngleRadians + settings.sweepAngleRadians);
 
-// // Appending to the DOM
-// const logo = document.createElement('img')
-// logo.src = webpackLogo
+    // move to centre
+    d += "M" + settings.centreX + "," + settings.centreY + " ";
+    // line to first edge
+    d += "L" + firstCircumferenceX + "," + firstCircumferenceY + " ";
+    // arc
+    // Radius X, Radius Y, X Axis Rotation, Large Arc Flag, Sweep Flag, End X, End Y
+    d += "A" + settings.radius + "," + settings.radius + " 0 0,1 " + secondCircumferenceX + "," + secondCircumferenceY + " ";
+    // close path
+    d += "Z";
 
-// const heading = document.createElement('h1')
-// heading.textContent = example()
+    const arc = document.createElementNS("http://www.w3.org/2000/svg", "path");
 
-// // Test a background image url in CSS
-// const imageBackground = document.createElement('div')
-// imageBackground.classList.add('image')
+    arc.setAttributeNS(null, "d", d);
+    arc.setAttributeNS(null, "fill", settings.fillColour);
+    // arc.setAttributeNS(null, "style", "stroke:" + settings.strokeColour + ";");
 
-// // Test a public folder asset
-// const imagePublic = document.createElement('img')
-// imagePublic.src = '/assets/example.png'
+    document.getElementById(settings.id).appendChild(arc);
+}
+function pieChart() {
+    const total = data.reduce((a, b) => a + b);
+    const radiansPerUnit = (2 * Math.PI) / total;
 
-// const app = document.querySelector('#root')
-// app.append(logo, heading, imageBackground, imagePublic)
+    let startAngleRadians = -(2 * Math.PI) / 4;
+    let sweepAngleRadians = null;
+
+    for (let i = 0, l = data.length; i < l; i++) {
+        sweepAngleRadians = data[i] * radiansPerUnit;
+
+        drawPieSlice({ id: "svg", centreX: 256, centreY: 256, startAngleRadians: startAngleRadians, sweepAngleRadians: sweepAngleRadians, radius: radius[i], fillColour: colours[i], strokeColour: "#000000" });
+
+        startAngleRadians += sweepAngleRadians;
+    }
+}
+window.onload = function () {
+    pieChart();
+    document.querySelector('#svg').insertAdjacentHTML('beforeend', `<circle cx="256" cy="256" r="50" fill="white"/>`);
+}
